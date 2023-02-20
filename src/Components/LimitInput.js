580c9f92a0e4e6c540e5fb3254b7c636a7b81a04
Dennis Kimutai <kimutaidennisbrian@gmail.com>
2023-02-20 10:28:12 +0300

    Limit input layout

---
+++ src/Components/LimitInput.js
@@ -0,0 +1,11 @@
+const LimitInput = () => {
+  return (
+    <div className="limitinput">
+      <label className="custom-field">
+        <input type="number" placeholder="&nbsp;"/>
+        <span class="placeholder">Enter Limit</span>
+      </label>
+    </div>
+  );
+};
+export default LimitInput;
